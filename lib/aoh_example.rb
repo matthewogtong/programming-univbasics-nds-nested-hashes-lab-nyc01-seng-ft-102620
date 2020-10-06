acc243cbeefa26387a304b0b6855474c3e6b5fd9
Matthew Ogtong <ogtongm@gmail.com>
2020-10-06 17:22:48 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,12 +22,18 @@ def literal_aoh
   # held in the constants
 
   aoh = []
-  
+
   aoh.push(DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G)
 
 end
 
 def aoh_lookup(aoh, row, key)
+
+  aoh[row][key]
+
+
+
+
 end
 
 def aoh_update(aoh, row, key, new_value)
